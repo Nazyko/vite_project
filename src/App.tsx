71ac481571cdc2c1f680f2b347c4491d342ce222
Nazyko <reypnazarovanazira@gmail.com>
2024-12-19 14:46:46 +0500

    Изменила

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import NotFoundPage from './pages/NotFoundPage';
 import SinglePage from './pages/SinglePage';
-import { fetchProducts } from './store/cardsSlice';
-import { useAppDispatch } from './store/hook';
 
 const App = () => {    
     return (
